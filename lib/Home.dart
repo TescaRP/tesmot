3f6008f9fdc6cfa46e455fb05bffedc374ca1e2a
ooncyber <gabriel.brutu@gmail.com>
2020-01-14 13:27:24 -0300

    Projeto inicial + exemplo

---
+++ lib/Home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Imagem.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 50,),
+          //widget data
+          //widget imagem
+          Imagem()
+        ],
+      ),
+    );
+  }
+}
