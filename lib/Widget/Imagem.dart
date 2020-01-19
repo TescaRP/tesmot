81d393e4884d76efcd61e2ef9737ff12da00220c
Gabriel R Carneiro <gabriel.brutu@gmail.com>
2020-01-19 10:10:23 -0800

    Feature da imagem realizada

--- lib/Widget/Imagem.dart
+++ lib/Widget/Imagem.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
-Widget Imagem(){
-  return Text("Imagem");
+Widget Imagem() {
+  return Container(
+      margin: EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: new BorderRadius.circular(8.0),
+        child: Image.network(
+          'https://i.ytimg.com/vi/XvZk6dzmaxw/maxresdefault.jpg',
+        ),
+      ));
 }
