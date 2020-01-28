fc97c66b52ff8ade5e93f7d8ef1de9cc2030add2
Gabriel R Carneiro <gabriel.brutu@gmail.com>
2020-01-27 23:16:10 -0800

    adicionado o inicio da pagina

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tesmot/pages/Config.dart';
 import 'Widget/Imagem.dart';
 import 'Widget/Data.dart';
 
@@ -7,26 +8,34 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-  const dourado = Color(0xffFFD700);
+    const dourado = Color(0xffFFD700);
 
     return Scaffold(
       appBar: AppBar(
-      title: Center(child: Text("TESMOT")),
-      backgroundColor: dourado,
-    ),
+        title: Center(child: Text("TESMOT")),
+        backgroundColor: dourado,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.settings),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ConfigPage()));
+        },
+      ),
       body: Column(
         children: <Widget>[
-          SizedBox(height: 50,),
-          Container( 
-                    width: 300,
-                    height: 100,
-                    alignment: Alignment.centerRight,
-                    child: Data(),
-                ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: 300,
+            height: 100,
+            alignment: Alignment.centerRight,
+            child: Data(),
+          ),
           Imagem()
         ],
       ),
